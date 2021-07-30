f4eb9f71a0b1508a7b726f5cab9ecbe11b8b1ac9
AnthonyDWeb <delforge.anthony@gmail.com>
2021-07-30 13:37:26 +0200

    choice color pawn

--- src/components/Table.js
+++ src/components/Table.js
@@ -21,6 +21,7 @@ class Table extends React.Component {
 
     componentDidMount () {
         console.log(this.state.col1)
+
     }
 
     changeClass = (e)=> {
@@ -47,30 +48,29 @@ class Table extends React.Component {
                 })
                 break;
             }
-
         }
     }
     
-
-
-
+    victoryCondition = () =>{
+        
+    }
 
 
     render() {
         console.log(this.state)
         return (
-            <div className={table.div} >
+            <div className={table.divContainer} >
                 <table className={table.table} >
                     <tbody className={table.tbody} >
                         <tr className={table.tr}>
-                            <Button id='1'  class={this.state.col1[5]}/>
+                            <Button  class={this.state.col1[5]}/>
                             <Button  class={this.state.col2[5]}/>
                             <Button  class={this.state.col3[5]}/>
                             <Button  class={this.state.col4[5]}/>
                             <Button  class={this.state.col5[5]}/>
                             <Button  class={this.state.col6[5]}/>
                             <Button  class={this.state.col7[5]}/>
-                           </tr>
+                        </tr>
                         <tr className={table.tr}>
                             <Button  class={this.state.col1[4]}/>
                             <Button  class={this.state.col2[4]}/>
@@ -79,7 +79,7 @@ class Table extends React.Component {
                             <Button  class={this.state.col5[4]}/>
                             <Button  class={this.state.col6[4]}/>
                             <Button  class={this.state.col7[4]}/>
-                           </tr>
+                        </tr>
                         <tr className={table.tr}>
                             <Button  class={this.state.col1[3]}/>
                             <Button  class={this.state.col2[3]}/>
@@ -88,7 +88,7 @@ class Table extends React.Component {
                             <Button  class={this.state.col5[3]}/>
                             <Button  class={this.state.col6[3]}/>
                             <Button  class={this.state.col7[3]}/>
-                           </tr>
+                        </tr>
                         <tr className={table.tr}>
                             <Button  class={this.state.col1[2]}/>
                             <Button  class={this.state.col2[2]}/>
@@ -97,7 +97,7 @@ class Table extends React.Component {
                             <Button  class={this.state.col5[2]}/>
                             <Button  class={this.state.col6[2]}/>
                             <Button  class={this.state.col7[2]}/>
-                           </tr>
+                        </tr>
                         <tr className={table.tr}>
                             <Button  class={this.state.col1[1]}/>
                             <Button  class={this.state.col2[1]}/>
@@ -106,7 +106,7 @@ class Table extends React.Component {
                             <Button  class={this.state.col5[1]}/>
                             <Button  class={this.state.col6[1]}/>
                             <Button  class={this.state.col7[1]}/>
-                           </tr>
+                        </tr>
                         <tr className={table.tr}>
                             <Button  class={this.state.col1[0]}/>
                             <Button  class={this.state.col2[0]}/>
@@ -115,44 +115,20 @@ class Table extends React.Component {
                             <Button  class={this.state.col5[0]}/>
                             <Button  class={this.state.col6[0]}/>
                             <Button  class={this.state.col7[0]}/>
-                           </tr>
-                           <tr>
-                            <td className='table.flex'>
-                                <button onClick={()=>this.onClickColumn("col1")}>1</button></td>
-                               <td>
-                                <button onClick={()=>this.onClickColumn("col2")}>
-                                   2
-                               </button></td>
-                               <td>
-                                <button onClick={()=>this.onClickColumn("col3")}>
-                                   3
-                               </button></td>
-                               <td>
-                                <button onClick={()=>this.onClickColumn("col4")}>
-                                   4
-                               </button></td>
-                               <td>
-                                <button onClick={()=>this.onClickColumn("col5")}>
-                                   5
-                               </button></td>
-                               <td>
-                                <button onClick={()=>this.onClickColumn('col6')}>
-                                   6
-                               </button></td>
-                               <td>
-                                <button onClick={()=>this.onClickColumn("col7")}>
-                                   7
-                               </button></td>
-                               
-                           </tr>
-                        
-                        
-                    </tbody>
+                        </tr>
+                        <tr>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn("col1")}>1</button></td>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn("col2")}>2</button></td>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn("col3")}>3</button></td>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn("col4")}>4</button></td>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn("col5")}>5</button></td>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn('col6')}>6</button></td>
+                            <td><button className={table.btnChoice} onClick={()=>this.onClickColumn("col7")}>7</button></td>   
+                        </tr>
+                        </tbody>
                 </table>
             </div>
-
         )
-
     }
 }
 
