81d2fed39355dbce482a871fa8d1fdff0d099b1f
AnthonyDWeb <delforge.anthony@gmail.com>
2021-08-02 09:53:04 +0200

    add asset

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import './App.css';
 import Table from './components/Table';
 
+import casque from './asset/casque6.png'
+
 class App extends React.Component {
   render() {
     return (
-        <div>
+        <div className="mainContent">
+            <img className="upImgTitle" src={casque} alt="" />
             <h1 className="mainTitle">3BigBoys</h1>
-            <div className="div">
                 <Table />
-            </div>
+            {/* </div> */}
             <p className="bottomLine">C'est a vous de jouer !!!!!!!!!!!!!!!</p>
         </div>
 
